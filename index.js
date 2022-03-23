f169727e129853090402ffa3ecab1252838e13e5
Chase <sccala18@gmail.com>
2022-03-22 17:35:15 -0700

    add mobile nav

---
+++ index.js
@@ -0,0 +1,15 @@
+// When somenoe clicks the hambeurger button
+// if the nav is clossed, open it
+// if the nav is open, close it
+
+const nav = document.querySelector('.primary-navigation')
+const navToggle = document.querySelector('.mobile-nav-toggle')
+
+navToggle.addEventListener('click', () => {
+  const visibility = nav.getAttribute('data-visible')
+  if (visibility === 'false') {
+    nav.setAttribute('data-visible', 'true')
+  } else {
+    nav.setAttribute('data-visible', 'false')
+  }
+})
