d03ba55fb27e30c189a216c3a3993f609f4c2400
Chase <sccala18@gmail.com>
2022-03-25 11:12:55 -0700

    refactor if statement

--- tabs.js
+++ tabs.js
@@ -15,24 +15,22 @@ function changeTabFocus(e) {
   console.log(e.keyCode)
   if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
     tabs[tabFocus].setAttribute('tabindex', -1)
-  }
 
-  if (e.keyCode === keydownRight) {
-    tabFocus++
-    if (tabFocus >= tabs.length) {
-      tabFocus = 0
+    if (e.keyCode === keydownRight) {
+      tabFocus++
+      if (tabFocus >= tabs.length) {
+        tabFocus = 0
+      }
+    } else if (e.keyCode === keydownLeft) {
+      tabFocus--
+      if (tabFocus < 0) {
+        tabFocus = tabs.length - 1
+      }
     }
-  }
 
-  if (e.keyCode === keydownLeft) {
-    tabFocus--
-    if (tabFocus < 0) {
-      tabFocus = tabs.length - 1
-    }
+    tabs[tabFocus].setAttribute('tabindex', 0)
+    tabs[tabFocus].focus()
   }
-
-  tabs[tabFocus].setAttribute('tabindex', 0)
-  tabs[tabFocus].focus()
 }
 
 function changeTabPanel(e) {
@@ -48,7 +46,7 @@ function changeTabPanel(e) {
     .setAttribute('aria-selected', false)
 
   targetTab.setAttribute('aria-selected', true)
-  
+
   hideContent(mainContainer, '[role="tabpanel"]')
   showContent(mainContainer, [`#${targetPanel}`])
   hideContent(mainContainer, 'picture')
