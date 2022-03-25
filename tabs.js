40290805798fe3c59d767cebfdb8bda1e1ac8165
Chase <sccala18@gmail.com>
2022-03-24 21:40:09 -0700

    tab keydown event

--- tabs.js
+++ tabs.js
@@ -2,24 +2,31 @@
 const tabList = document.querySelector('[role="tablist"]')
 const tabs = tabList.querySelectorAll('[role="tab"]')
 
-let tabFocus = 0
+tabList.addEventListener('keydown', changeTabFocus)
 
-tabList.addEventListener('keydown', (e) => {
+let tabFocus = 0
+function changeTabFocus(e) {
   const keydownLeft = 37
   const keydownRight = 39
-
+  console.log(e.keyCode)
   if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
     tabs[tabFocus].setAttribute('tabindex', -1)
   }
 
   if (e.keyCode === keydownRight) {
     tabFocus++
+    if (tabFocus >= tabs.length) {
+      tabFocus = 0
+    }
   }
 
   if (e.keyCode === keydownLeft) {
     tabFocus--
+    if (tabFocus < 0) {
+      tabFocus = tabs.length - 1
+    }
   }
 
-  tab[tabFocus].setAttribute('tabindex', 0)
-  tab[tabFocus].focus()
-})
+  tabs[tabFocus].setAttribute('tabindex', 0)
+  tabs[tabFocus].focus()
+}
