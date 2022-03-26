945a7ebc713fc84a30781afe36445293f012e346
Chase <sccala18@gmail.com>
2022-03-25 23:28:30 -0700

    add technology button js

--- button.js
+++ button.js
@@ -1,52 +1,52 @@
 // @ts-nocheck
-const tabList = document.querySelector('[role="tablist"]')
-const tabs = tabList.querySelectorAll('[role="tab"]')
+const buttonList = document.querySelector('[role="buttonlist"]')
+const buttons = buttonList.querySelectorAll('[role="button"]')
 
-tabList.addEventListener('keydown', changeTabFocus)
+buttonList.addEventListener('keydown', changeTabFocus)
 
-tabs.forEach((tab) => {
+buttons.forEach((tab) => {
   tab.addEventListener('click', changeTabPanel)
 })
 
-let tabFocus = 0
+let buttonFocus = 0
 function changeTabFocus(e) {
   const keydownLeft = 37
   const keydownRight = 39
   if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
-    tabs[tabFocus].setAttribute('tabindex', -1)
+    buttons[buttonFocus].setAttribute('tabindex', -1)
 
     if (e.keyCode === keydownRight) {
-      tabFocus++
-      if (tabFocus >= tabs.length) {
-        tabFocus = 0
+      buttonFocus++
+      if (buttonFocus >= buttons.length) {
+        buttonFocus = 0
       }
     } else if (e.keyCode === keydownLeft) {
-      tabFocus--
-      if (tabFocus < 0) {
-        tabFocus = tabs.length - 1
+      buttonFocus--
+      if (buttonFocus < 0) {
+        buttonFocus = buttons.length - 1
       }
     }
 
-    tabs[tabFocus].setAttribute('tabindex', 0)
-    tabs[tabFocus].focus()
+    buttons[buttonFocus].setAttribute('tabindex', 0)
+    buttons[buttonFocus].focus()
   }
 }
 
-function changeTabPanel(e) {
-  const targetTab = e.target
-  const targetPanel = targetTab.getAttribute('aria-controls')
-  const targetImage = targetTab.getAttribute('data-image')
+function changeButtonPanel(e) {
+  const targetButton = e.target
+  const targetPanel = targetButton.getAttribute('aria-controls')
+  const targetImage = targetButton.getAttribute('data-image')
 
-  const tabContainer = targetTab.parentNode
-  const mainContainer = tabContainer.parentNode
+  const buttonContainer = targetButton.parentNode
+  const mainContainer = buttonContainer.parentNode
 
-  tabContainer
+  buttonContainer
     .querySelector('[aria-selected="true"]')
     .setAttribute('aria-selected', false)
 
-  targetTab.setAttribute('aria-selected', true)
+  targetButton.setAttribute('aria-selected', true)
 
-  hideContent(mainContainer, '[role="tabpanel"]')
+  hideContent(mainContainer, '[role="buttonlist"]')
   showContent(mainContainer, [`#${targetPanel}`])
   hideContent(mainContainer, 'picture')
   showContent(mainContainer, [`#${targetImage}`])
